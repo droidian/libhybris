88236d89490a74846836e37df7d85b7d6f8edd1b
Alexander Rutz <alex@familyrutz.com>
2026-07-23 05:50:20 -0600

    Revert "lindroid-drm: avoid BO creation for Wayland clients"
    This reverts commit b22d041aaf97c5e2bf7ea9a4281fdf9b73edd805.

--- hybris/platforms/wayland/wayland_window_common.h
+++ hybris/platforms/wayland/wayland_window_common.h
@@ -165,6 +165,8 @@ public:
               struct wl_display *display,
               struct wl_event_queue *queue) override;
 
+    struct gbm_bo *bo;
+    int dmabuf_fd;
     struct zwp_linux_dmabuf_v1 *wl_dmabuf;
 };
 #endif
